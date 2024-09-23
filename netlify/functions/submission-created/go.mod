7c39870ecd5821dd2a36256b734383cfbdce1389
bth <gh@hautogdoad.com>
2024-09-23 15:13:31 -0700

    mailgun-go working locally with test domain
    the library brings in a lot of crap, might not keep it
    
    but got the JSON and ENV vars working

--- netlify/functions/submission-created/go.mod
+++ netlify/functions/submission-created/go.mod
@@ -2,4 +2,15 @@ module github.com/brtholomy/netlify.go
 
 go 1.23.1
 
-require github.com/aws/aws-lambda-go v1.47.0
+require (
+	github.com/aws/aws-lambda-go v1.47.0
+	github.com/mailgun/mailgun-go/v4 v4.15.2
+)
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+)
--- netlify/functions/submission-created/main.go
+++ netlify/functions/submission-created/main.go
@@ -1,14 +1,58 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/mailgun/mailgun-go/v4"
+	"os"
+	"time"
 )
 
+func AddListMember(domain, apiKey, email string) error {
+	mg := mailgun.NewMailgun(domain, apiKey)
+
+	member := mailgun.Member{
+		Address:    email,
+		Subscribed: mailgun.Subscribed,
+	}
+	mailinglist := "test@" + domain
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	return mg.CreateMember(ctx, true, mailinglist, member)
+}
+
+// easiest to just define exactly what I need:
+// https://gobyexample.com/json
+type Payload struct {
+	Email string
+}
+
+type Body struct {
+	Payload Payload
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var data Body
+	if err := json.Unmarshal([]byte(request.Body), &data); err != nil {
+		panic(err)
+	}
+	email := data.Payload.Email
+	fmt.Println(email)
+
+	domain := os.Getenv("MAILGUN_TEST_DOMAIN")
+	apiKey := os.Getenv("MAILGUN_TEST_API_KEY")
+	if err := AddListMember(domain, apiKey, email); err != nil {
+		panic(err)
+	}
+
+	// TODO: serve the /subscribe/confirm page:
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Foobar bth forever! and ever!!",
 	}, nil
 }
 
