37caa2210f661e0060bf3e9b053e5eb96ceee6fc
bth <gh@hautogdoad.com>
2026-08-13 10:43:35 -0700

    scripts : paths

--- scripts/hugonew.zsh
+++ scripts/hugonew.zsh
@@ -5,9 +5,11 @@
 function hugonew {
     [[ -z $1 ]] && echo "usage: $0 NAME" && exit 1
 
-    local DIR="${0:A:h}"
-    cd ../content/posts || exit 1
-    local NEXT=`um next $1`
+    local SCRIPTSDIR="${0:A:h}"
+    local ROOTDIR=$SCRIPTSDIR/..
+    cd $ROOTDIR/content/posts || exit 1
+    local NEXT=content/posts/`um next $1`
+    cd $ROOTDIR
     hugo new $NEXT || exit 1
     emacsclient -n $NEXT
     cd -
